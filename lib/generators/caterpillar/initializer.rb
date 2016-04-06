506f8bf62c2c276a1e5c5de70f6124a448fe3624
Nitish Sharma <nitish.sharma@keepworks.com>
2016-04-06 18:15:10 +0530

    Correct base url

--- lib/generators/caterpillar/initializer.rb
+++ lib/generators/caterpillar/initializer.rb
@@ -1,5 +1,5 @@
 Caterpillar.configure do |config|
   config.api_key = 'YOUR_CATERPILLAR_API_KEY'
   config.api_version = 'v1'
-  config.base_uri = 'https://caterpillar.io/api'
+  config.base_uri = 'https://api.caterpillar.io'
 end
