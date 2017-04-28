1968610086fe8d8ac1d53b7cc8e0468c3781265b
Nitish Sharma <nitish.sharma@keepworks.com>
2017-04-28 12:40:01 +0530

    Fix encoding (#3)
    * Fix encoding
    
    * Fix version

--- lib/caterpillar-ruby.rb
+++ lib/caterpillar-ruby.rb
@@ -31,7 +31,7 @@ module Caterpillar
         }.merge!(options)
       }
 
-      response = HTTParty.post("#{base_uri}/#{api_version}/documents/convert", query: query).force_encoding('UTF-8')
+      response = HTTParty.post("#{base_uri}/#{api_version}/documents/convert", query: query).force_encoding('ISO-8859-1').encode('UTF-8')
 
       parsed_response = response.is_caterpillar_json?
       raise APIError.new(parsed_response['message']) if parsed_response.present? && parsed_response['status'] == 0
